71fff4e3376abf371f48ab61b22bd5f422d90da0
Pedro-Afonso <pedro.766@hotmail.com>
2022-09-15 15:11:58 -0300

    feat: Returns the url parameters

--- src/shared/hooks/useSearch.ts
+++ src/shared/hooks/useSearch.ts
@@ -1,41 +1,16 @@
-import { useEffect, useRef, useState } from 'react'
+import { useMemo } from 'react'
+import { useSearchParams } from 'react-router-dom'
 
-const api = 'http://localhost:5000/api/users'
+export const useSearch = () => {
+  const [searchParams] = useSearchParams()
 
-export const useSearch = <T>(search: string, page = 1, limit = 10) => {
-  const [users, setUsers] = useState<T[] | null>(null)
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState<string>('')
-  const isCancelled = useRef(false)
-
-  useEffect(() => {
-    const searchData = async () => {
-      try {
-        if (isCancelled.current) return
-        setLoading(true)
-
-        const url = `${api}/search?q=${search}&page=${page}&limit=${limit}`
-
-        const data = await fetch(url)
-          .then(res => res.json())
-          .then(res => res as { users: T[]; numberOfUsers: number })
-
-        setUsers(data.users)
-      } catch (error) {
-        setError('Ocorreu um erro, tente novamente mais tarde.')
-      } finally {
-        setLoading(false)
-      }
+  return useMemo(() => {
+    return {
+      q: searchParams.get('q') || '',
+      page: +(searchParams.get('page') || 1),
+      limit: +(searchParams.get('limit') || 9),
+      state: searchParams.get('location_state') || '',
+      sort: searchParams.get('sort') || 'first_name'
     }
-
-    searchData()
-  }, [search, limit, page])
-
-  useEffect(() => {
-    return () => {
-      isCancelled.current = true
-    }
-  }, [])
-
-  return { users, loading, error }
+  }, [searchParams])
 }
